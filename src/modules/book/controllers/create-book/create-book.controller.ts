fedfc3d0bc8d6c9c090520ca78d4c7e09ff8d741
Natan <zerogamerptbr@gmail.com>
2024-01-29 15:08:51 -0300

    :recycle: refac: removing the jwt validation on the createBook route

--- src/modules/book/controllers/create-book/create-book.controller.ts
+++ src/modules/book/controllers/create-book/create-book.controller.ts
@@ -1,10 +1,8 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateBookService } from '../../services/create-book/create-book.service';
 import { CreateBookDTO } from '../../dtos';
-import { JwtGuard } from 'src/modules/auth/guard';
 
 @Controller('book')
-@UseGuards(JwtGuard)
 export class CreateBookController {
   constructor(private createBookService: CreateBookService) {}
 
