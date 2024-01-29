dd4a3fdeb8ff77c68a5c7072d05e0e6b62508510
Natan <zerogamerptbr@gmail.com>
2024-01-29 15:25:35 -0300

    :label: types: adding types to functions

--- src/modules/book/services/create-book/create-book.service.ts
+++ src/modules/book/services/create-book/create-book.service.ts
@@ -1,11 +1,12 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../prisma/services/prisma/prisma.service';
 import { CreateBookDTO } from '../../dtos';
+import { Book } from '@prisma/client';
 
 @Injectable()
 export class CreateBookService {
   constructor(private prisma: PrismaService) {}
-  async execute({ title, author }: CreateBookDTO) {
+  async execute({ title, author }: CreateBookDTO): Promise<Book> {
     const bookAlreadyExists = await this.prisma.book.findUnique({
       where: { title },
     });
