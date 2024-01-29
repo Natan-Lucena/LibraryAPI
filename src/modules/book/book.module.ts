ffedaf6a876f6c6d824c0598b26810a6187a5a2f
Natan <zerogamerptbr@gmail.com>
2024-01-29 15:26:28 -0300

    :sparkles: feat: book module using get book service and controller

--- src/modules/book/book.module.ts
+++ src/modules/book/book.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CreateBookService } from './services/create-book/create-book.service';
 import { CreateBookController } from './controllers/create-book/create-book.controller';
+import { GetBooksService } from './services/get-books/get-books.service';
+import { GetBooksController } from './controllers/get-books/get-books.controller';
 
 @Module({
-  providers: [CreateBookService],
-  controllers: [CreateBookController]
+  providers: [CreateBookService, GetBooksService],
+  controllers: [CreateBookController, GetBooksController]
 })
 export class BookModule {}
