81b0ff93d771a38ad8b38f19e6f681cb71f67dc8
Natan <zerogamerptbr@gmail.com>
2024-01-29 14:21:20 -0300

    :sparkles: feat: app module using book module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
+import { BookModule } from './modules/book/book.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { AuthModule } from './modules/auth/auth.module';
     }),
     PrismaModule,
     AuthModule,
+    BookModule,
   ],
   controllers: [AppController],
   providers: [AppService],
